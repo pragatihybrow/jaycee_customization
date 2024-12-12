0287c8fbd8ca3d5baa0bfa7fd72474e2858a3d77
pragatid20 <pragati.d@logicaldna.com>
2024-12-12 13:36:27 +0530

    |jaycee_customization|value and sales invoice will get fetch in childtable based on sales order|

--- jaycee_customization/public/js/shipment.js
+++ jaycee_customization/public/js/shipment.js
@@ -1,18 +1,15 @@
-
-
 frappe.ui.form.on('Shipment', {
-    refresh: function (frm) {
-        // Add the "Get Items " button for draft documents
+    refresh: function(frm) {
         if (frm.doc.docstatus === 0) {
             frm.add_custom_button(
                 __("Sales Order"), 
                 () => frm.events.get_items_from_sales_order(frm),
-                __("Get Items From") 
+                __("Get Items From")
             );
         }
     },
 
-    get_items_from_sales_order: function (frm) {
+    get_items_from_sales_order: function(frm) {
         erpnext.utils.map_current_doc({
             method: "jaycee_customization.override.shipment.custom_make_material_request",
             source_doctype: "Sales Order",  
@@ -29,5 +26,23 @@ frappe.ui.form.on('Shipment', {
             },
         });
     },
-});
 
+    validate: function(frm) {
+        let sales_order_ids = frm.doc.custom_shipment_sales_order_.map(item => item.sales_order);        
+        if (sales_order_ids.length > 0) {
+            frappe.db.get_list('Sales Invoice', {
+                filters: { 'custom_sales_order': ['in', sales_order_ids] },
+                fields: ['name', 'custom_sales_order'] 
+            }).then(function(sales_invoices) {                
+                let sales_invoice_map = {}; 
+                sales_invoices.forEach(invoice => {
+                    sales_invoice_map[invoice.custom_sales_order] = invoice.name; 
+                });                
+                frm.doc.custom_shipment_sales_order_.forEach((item, index) => {
+                    item.sales_invoice = sales_invoice_map[item.sales_order]; 
+                    frm.refresh_field('custom_shipment_sales_order_');
+                });
+            });
+        }
+    }
+});
